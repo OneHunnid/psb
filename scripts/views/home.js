c6fe6aa00f4de518181a37ded296ec2b8424927c
Chris Limbrick <climbrick1989gmail.com@>
2015-12-22 11:22:05 -0500

    adding spinner icon to tools for slow loading

--- scripts/views/home.js
+++ scripts/views/home.js
@@ -30,6 +30,28 @@ var Home = (function() {
 		desc: 'If you know a tool that should be added to our list, send a tweet to @climbrick.'
 	};
 
+	var opts = {
+		  lines: 13 // The number of lines to draw
+		, length: 28 // The length of each line
+		, width: 14 // The line thickness
+		, radius: 42 // The radius of the inner circle
+		, scale: .15 // Scales overall size of the spinner
+		, corners: 1 // Corner roundness (0..1)
+		, color: '#000' // #rgb or #rrggbb or array of colors
+		, opacity: 0.25 // Opacity of the lines
+		, rotate: 0 // The rotation offset
+		, direction: 1 // 1: clockwise, -1: counterclockwise
+		, speed: 1 // Rounds per second
+		, trail: 60 // Afterglow percentage
+		, fps: 20 // Frames per second when using setTimeout() as a fallback for CSS
+		, zIndex: 2e9 // The z-index (defaults to 2000000000)
+		, className: 'spinner' // The CSS class to assign to the spinner
+		, top: '50%' // Top position relative to parent
+		, left: '50%' // Left position relative to parent
+		, shadow: false // Whether to render a shadow
+		, hwaccel: false // Whether to use hardware acceleration
+		, position: 'absolute' // Element positioning
+	}
 
 	// partials
 	var homePartial = $('#partial-banner-home').html();
@@ -138,7 +160,13 @@ var Home = (function() {
 		});
 	}
 
+	function setSpinner() {
+		var target = document.getElementById('tools')
+		var spinner = new Spinner(opts).spin(target);
+	}
+
 	setActive();
+	setSpinner();
 
 	// KICKSTART VIEW
 	function initHome() {
